ca67238d179e8512c48ad0ec2610961ff636e45d
theylorantunes <theylorantunes@gmail.com>
2026-05-25 12:42:59 -0300

    fix: bloqueio de css por segurança

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,5 +22,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Pedido::observe(PedidoObserver::class);
+
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
