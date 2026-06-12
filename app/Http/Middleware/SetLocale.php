5502f230c9ae7a4294cf2fc0e7a9da4b684a5af4
theylorantunes <theylorantunes@gmail.com>
2026-06-12 11:58:43 -0300

    feat: botão para alterar idioma adicionado no header (i18n)

--- app/Http/Middleware/SetLocale.php
+++ app/Http/Middleware/SetLocale.php
@@ -11,17 +11,12 @@ class SetLocale
 {
     public function handle(Request $request, Closure $next)
     {
-        // 1. Se o usuário já tiver forçado uma troca de idioma na loja (via um botão), usamos a sessão
-        if (Session::has('locale')) {
-            App::setLocale(Session::get('locale'));
+        if (session()->has('locale')) {
+            app()->setLocale(session()->get('locale'));
         } 
-        // 2. Se não, nós lemos a configuração do navegador dele!
+
         else {
-            // O Laravel lê o cabeçalho 'Accept-Language' do navegador.
-            // Passamos um array com os idiomas que a nossa loja suporta: ['pt', 'en']
-            $idiomaNavegador = $request->getPreferredLanguage(['pt', 'en']);
-            
-            App::setLocale($idiomaNavegador);
+            app()->setLocale('pt');
         }
 
         return $next($request);
