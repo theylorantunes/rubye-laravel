6c8a91df41dfb71e45d251e862303bff7432fe8c
theylorantunes <theylorantunes@gmail.com>
2026-06-12 12:17:23 -0300

    fix: nome no bd diferindo o da migration

--- database/migrations/2026_06_12_133951_altera_tamanho_imagem_produtos.php
+++ database/migrations/2026_06_12_133951_altera_tamanho_imagem_produtos.php
@@ -7,13 +7,14 @@ return new class extends Migration
 {
     public function up()
     {
+        // Altera as colunas para caberem textos gigantes (Base64)
         DB::statement('ALTER TABLE produtos MODIFY imagem LONGTEXT');
-        DB::statement('ALTER TABLE colecoes MODIFY imagem_capa LONGTEXT');
+        DB::statement('ALTER TABLE colecoes MODIFY imagem LONGTEXT'); // <-- Corrigido para 'imagem'
     }
 
     public function down()
     {
         DB::statement('ALTER TABLE produtos MODIFY imagem VARCHAR(255)');
-        DB::statement('ALTER TABLE colecoes MODIFY imagem_capa VARCHAR(255)');
+        DB::statement('ALTER TABLE colecoes MODIFY imagem VARCHAR(255)'); // <-- Corrigido para 'imagem'
     }
 };
